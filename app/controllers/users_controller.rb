5dbc718f8f9a1bd3a3c0b806f6fecb8c357836f4
subratrout <subratnrout@gmail.com>
2015-12-04 14:47:39 -0800

    Make sure correct user sign in have access to its page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
   def index
     @users = User.all
   end
@@ -46,6 +47,10 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    def require_correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
     end
 
 end
