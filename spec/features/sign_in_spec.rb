5767b3482ce83169a22491912e54ff23fad75d73
subratrout <subratnrout@gmail.com>
2015-12-04 12:03:02 -0800

    Storing current_user id in session and showing current user name in view page

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -28,6 +28,9 @@ describe "Signing in" do
     expect(current_path).to eq(user_path(user))
 
     expect(page).to have_text("Welcome back, #{user.name}!")
+    expect(page).to have_link(user.name)
+    expect(page).not_to have_link('Sign In')
+    expect(page).not_to have_link('Sign Up')
   end
 
   it "does not sign in the user if the email/password combination is invalid" do
@@ -43,5 +46,8 @@ describe "Signing in" do
     click_button 'Sign In'
 
     expect(page).to have_text('Invalid')
+    expect(page).not_to have_link(user.name)
+    expect(page).to have_link('Sign In')
+    expect(page).to have_link('Sign Up')
   end
 end
