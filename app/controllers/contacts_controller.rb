53e80bdd2b866025a031fc9e86249e9d97ead132
subratrout <subratnrout@gmail.com>
2015-12-05 18:09:08 -0800

    Created new action and view templates for contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,26 @@ class ContactsController < ApplicationController
     @user = User.find(params[:user_id])
     @contacts = @user.contacts
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @contact = @user.contacts.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @contact = @user.contacts.new(contact_params)
+    if @contact.save
+      redirect_to user_contacts_path(@user),
+            notice: "Contacts added"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :phone, :address)
+  end
 end
---
+++ spec/features/create_contact_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Creating a new contact" do
+  it "saves the contact" do
+    user = User.create(user_attributes)
+
+    visit user_url(user)
+
+    click_link 'Add contact'
+
+    expect(current_path).to eq(new_user_contact_path(user))
+
+    fill_in "Name", with: "Ryan Fox"
+    fill_in "Email", with: "ryan@icentris.com"
+    fill_in "Phone", with: "234-567-8901"
+    fill_in "Address", with: "707 wood cross, Utah"
+
+    click_button 'Add contact'
+
+    expect(current_path).to eq(user_contacts_path(user))
+
+    expect(page).to have_text("Contact added!")
+  end
+
+  it "does not save the contact if it's invalid" do
+    user = User.create(user_attributes)
+
+    visit new_user_contact_url(user)
+
+    expect {
+      click_button 'Add contact'
+    }.not_to change(contact, :count)
+
+    expect(page).to have_text('error')
+  end
+end
